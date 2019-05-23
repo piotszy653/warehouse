0037e138099baf58825cece38cee106247e464d9
Piotr <piotszy653@student.polsl.pl>
2019-05-23 18:53:47 +0200

    added Client class with accessors

--- Client.java
+++ Client.java
@@ -13,8 +13,40 @@ public class Client implements Serializable {
     @Column(nullable = false)
     private String name;
 
+    private String mail = null;
+
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id=id;
+    }
 
     public String getName(){
         return name;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public String getMail(){
+        return mail;
+    }
+
+    public void setMail(String mail){
+        this.mail=mail;
+    }
+
+
+    @Override
+    public String toString(){
+       String str = id+"name of Client: "+name;
+
+       if(mail!=null) str += (", mail: " + mail);
+
+       return str;
     }
 }
